4290a53681ca32cc443e6261bc42a6ba0d1018a7
markaelie <66760217+markaelie@users.noreply.github.com>
2022-02-17 18:09:57 +0400

    upt

---
+++ SmoothFollow-Unity/Assets/GizmosController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshCollider))]
+
+public class GizmosController : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+}
---
+++ SmoothFollow-Unity/Assets/UrlButton.cs
@@ -0,0 +1,10 @@
+﻿  using UnityEngine;
+  using System.Collections;
+  
+  public class UrlButton : MonoBehaviour
+  {
+      public void OpenURL()
+      {
+          Application.OpenURL("https://github.com/markaelie");
+      }
+  }
