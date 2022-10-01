b4aee81c2a5ff52dd49dacfb34758a3d60869dc7
markhuna <markhuna@gmail.com>
2022-10-01 13:05:07 +0400

    u

---
+++ Smooth-Follow-Unity/Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,41 @@
+﻿//github.com/markulie
+
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SphereCollider))]
+public class SmoothFollow : MonoBehaviour
+{
+    public GameObject follower;
+    public Vector3 followerOffset = new Vector3(0, 0, 0);
+    public Slider sliderSpeed;
+    public Toggle lookAt;
+    public float speed;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        follower.transform.position = transform.position + followerOffset;
+    }
+
+    private void Update()
+    {
+        speed = sliderSpeed.value;
+        follower.transform.position = Vector3.Lerp(follower.transform.position, transform.position + followerOffset, speed);
+        if (lookAt.isOn) follower.transform.LookAt(transform.position);
+    }
+
+    private void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+}
---
+++ Smooth-Follow-Unity/Assets/Scripts/UrlButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UrlButton : MonoBehaviour
+{
+    public string url;
+
+    public void OpenURL()
+    {
+        Application.OpenURL(url);
+    }
+}
